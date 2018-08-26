8ae51bcda92f3d5e4cd49fc72918191d2e1cac02
MikePineda <mikepineda97@gmail.com>
2018-08-25 21:59:50 -0500

    rada - fix 9

--- bookmark.js
+++ bookmark.js
@@ -1,22 +1,30 @@
-javascript:(function(){cb=function() { create_spritz(); };
+javascript: ( function() {
 
-var script0=document.createElement('script0');
-script0.src='https://code.jquery.com/jquery-3.3.1.min.js';
-document.body.appendChild(script0);})();
+cb = function() {
+  create_spritz();
+}
 
-var script1=document.createElement('script1');
-script1.src='https://rawgit.com/MikePineda/HackMty2018/master/tracking/build/tracking-min.js';
-document.body.appendChild(script1);})();
+var script0 = document.createElement('SCRIPT');
+script0.src = 'https://code.jquery.com/jquery-3.3.1.min.js';
+document.body.appendChild(script0);
 
-var script2=document.createElement('script2');
-script2.src='https://rawgit.com/MikePineda/HackMty2018/master/tracking/build/data/face-min.js';
-document.body.appendChild(script2;})();
+var script1 = document.createElement('SCRIPT');
+script1.src = 'https://rawgit.com/MikePineda/HackMty2018/master/tracking/build/tracking-min.js';
+document.body.appendChild(script1);
 
-var script3=document.createElement('script3');
-script3.src='https://rawgit.com/MikePineda/HackMty2018/master/tracking/assets/stats.min.js';
-document.body.appendChild(script3);})();
+var script2 = document.createElement('SCRIPT');
+script2.src = 'https://rawgit.com/MikePineda/HackMty2018/master/tracking/build/data/face-min.js';
+document.body.appendChild(script2);
 
-var script4=document.createElement('script4');
-script4.src='https://rawgit.com/MikePineda/HackMty2018/master/glance/spritz.js?callback=cb';
-script4.onload=cb;
-document.body.appendChild(script4);})();
+var script3 = document.createElement('SCRIPT');
+script3.src = 'https://rawgit.com/MikePineda/HackMty2018/master/tracking/assets/stats.min.js';
+document.body.appendChild(script3);
+
+var script4 = document.createElement('SCRIPT');
+script4.src = 'https://rawgit.com/MikePineda/HackMty2018/master/glance/spritz.js?callback=cb';
+script4.onload = cb;
+document.body.appendChild(script4);
+}
+
+)();
+//javascript:(function(){cb=function() { create_spritz(); }; var script=document.createElement('SCRIPT');script.src='https://rawgit.com/MikePineda/HackMty2018/master/glance/spritz.js?callback=cb';script.onload=cb;document.body.appendChild(script);})();
