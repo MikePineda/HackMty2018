5c1f9cdd542aa4a48d683650f4c95f08f06b3af9
MikePineda <mikepineda97@gmail.com>
2018-08-25 22:05:06 -0500

    rada - fix 10

--- bookmark.js
+++ bookmark.js
@@ -2,7 +2,7 @@ javascript: ( function() {
 
 cb = function() {
   create_spritz();
-}
+};
 
 var script0 = document.createElement('SCRIPT');
 script0.src = 'https://code.jquery.com/jquery-3.3.1.min.js';
@@ -16,9 +16,9 @@ var script2 = document.createElement('SCRIPT');
 script2.src = 'https://rawgit.com/MikePineda/HackMty2018/master/tracking/build/data/face-min.js';
 document.body.appendChild(script2);
 
-var script3 = document.createElement('SCRIPT');
-script3.src = 'https://rawgit.com/MikePineda/HackMty2018/master/tracking/assets/stats.min.js';
-document.body.appendChild(script3);
+// var script3 = document.createElement('SCRIPT');
+// script3.src = 'https://rawgit.com/MikePineda/HackMty2018/master/tracking/assets/stats.min.js';
+// document.body.appendChild(script3);
 
 var script4 = document.createElement('SCRIPT');
 script4.src = 'https://rawgit.com/MikePineda/HackMty2018/master/glance/spritz.js?callback=cb';
