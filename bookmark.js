aa9f0e0a4a37edab4bae7345ca2c4ea2a6446801
MikePineda <mikepineda97@gmail.com>
2018-08-26 09:00:06 -0500

    testing new method

--- bookmark.js
+++ bookmark.js
@@ -9,19 +9,19 @@ script0.src = 'https://code.jquery.com/jquery-3.3.1.min.js';
 document.body.appendChild(script0);
 
 var script1 = document.createElement('SCRIPT');
-script1.src = 'https://rawgit.com/MikePineda/HackMty2018/master/tracking/build/tracking-min.js';
+script1.src = 'https://rawgit.com/MikePineda/HackMty2018/twitterSupport/tracking/build/tracking-min.js';
 document.body.appendChild(script1);
 
 var script2 = document.createElement('SCRIPT');
-script2.src = 'https://rawgit.com/MikePineda/HackMty2018/master/tracking/build/data/face-min.js';
+script2.src = 'https://rawgit.com/MikePineda/HackMty2018/twitterSupport/tracking/build/data/face-min.js';
 document.body.appendChild(script2);
 
 var script3 = document.createElement('SCRIPT');
-script3.src = 'https://rawgit.com/MikePineda/HackMty2018/master/fetch.js';
+script3.src = 'https://rawgit.com/MikePineda/HackMty2018/twitterSupport/fetch.js';
 document.body.appendChild(script3);
 
 var script4 = document.createElement('SCRIPT');
-script4.src = 'https://rawgit.com/MikePineda/HackMty2018/master/glance/spritz.js?callback=cb';
+script4.src = 'https://rawgit.com/MikePineda/HackMty2018/twitterSupport/glance/spritz.js?callback=cb';
 script4.onload = cb;
 document.body.appendChild(script4);
 }
